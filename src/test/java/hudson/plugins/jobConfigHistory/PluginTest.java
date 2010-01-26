eb2e8c8daa008477cd08c677122c1a0847c76507
mfriedenhagen <mfriedenhagen@dev.java.net>
2010-01-26 20:47:54 +0000

    Look for links

--- src/test/java/hudson/plugins/jobConfigHistory/PluginTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/PluginTest.java
@@ -4,22 +4,17 @@
 
 package hudson.plugins.jobConfigHistory;
 
-import hudson.model.FreeStyleProject;
-
 import java.io.IOException;
+import java.util.List;
 
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 import org.xml.sax.SAXException;
 
-import com.gargoylesoftware.htmlunit.Page;
-import com.gargoylesoftware.htmlunit.ScriptResult;
-import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
-import com.gargoylesoftware.htmlunit.javascript.host.Event;
-import com.gargoylesoftware.htmlunit.xml.XmlPage;
 
 /**
  * @author mfriedenhagen
@@ -62,16 +57,39 @@ public class PluginTest extends HudsonTestCase {
 
     @LocalData
     public void testSaveConfiguration() throws Exception {
-        final HtmlPage configuration1 = webClient.goTo("job/bar/configure");
-        assertXPath(configuration1, "//a[@href=\"/" + JOB_CONFIG_HISTORY_LINK + "\"]");
-        final HtmlForm configForm1 = configuration1.getFormByName("config");
-        final HtmlTextArea descriptionTextArea1 = configForm1.getTextAreaByName("description");
-        descriptionTextArea1.setText("just a test");
-        final HtmlPage jobPage = submit(configForm1);
-        final HtmlPage historyPage = goToJobConfigurationHistoryPage(jobPage);
-        System.out.println(historyPage.asXml());
+        final List<? extends HtmlAnchor> hrefs1 = setNewDescriptionAndGetHistoryLinks("just a test", "xml");
+        assertEquals(1, hrefs1.size());
+        final List<? extends HtmlAnchor> hrefs2 = setNewDescriptionAndGetHistoryLinks("just a second test", "xml");
+        assertEquals(2, hrefs2.size());
+        final List<? extends HtmlAnchor> hrefs3 = setNewDescriptionAndGetHistoryLinks("just a test", "raw");
+        assertEquals(3, hrefs3.size());
+        final List<? extends HtmlAnchor> hrefs4 = setNewDescriptionAndGetHistoryLinks("just a second test", "raw");
+        assertEquals(4, hrefs4.size());
+//        XmlPage page = (XmlPage) hrefs2.get(1).click();
+//        System.out.println(page.asText());
 //        final FreeStyleProject project = createFreeStyleProject("foo");
 //        final HtmlPage fooPage = webClient.getPage(project);
+    }
+
+    /**
+     * @param text
+     * @param type
+     * @return
+     * @throws IOException
+     * @throws SAXException
+     * @throws Exception
+     */
+    private List<? extends HtmlAnchor> setNewDescriptionAndGetHistoryLinks(final String text, final String type)
+            throws IOException, SAXException, Exception {
+        final HtmlPage configuration = webClient.goTo("job/bar/configure");
+        assertXPath(configuration, "//a[@href=\"/" + JOB_CONFIG_HISTORY_LINK + "\"]");
+        final HtmlForm configForm = configuration.getFormByName("config");
+        final HtmlTextArea descriptionTextArea = configForm.getTextAreaByName("description");
+        descriptionTextArea.setText(text);
+        final HtmlPage jobPage = submit(configForm);
+        final HtmlPage historyPage = goToJobConfigurationHistoryPage(jobPage);
+        final List<? extends HtmlAnchor> hrefs = (List<? extends HtmlAnchor>) historyPage.getByXPath("//a[contains(@href, \"configOutput?type=" + type +"\")]");
+        return hrefs;
     }
 
 }
