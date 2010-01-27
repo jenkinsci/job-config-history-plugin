db8ff88add1e535d49c10947196511b42e0bf1cd
mfriedenhagen <mfriedenhagen@dev.java.net>
2010-01-27 19:33:54 +0000

    Add assertions to JobConfigHistoryJobListenerTest

--- src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryJobListenerTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryJobListenerTest.java
@@ -6,7 +6,10 @@ package hudson.plugins.jobConfigHistory;
 
 import hudson.model.FreeStyleProject;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.xml.sax.SAXException;
@@ -16,21 +19,27 @@ import org.xml.sax.SAXException;
  *
  */
 public class JobConfigHistoryJobListenerTest extends HudsonTestCase {
-    private WebClient webClient;
+
+    private File jobsDir;
 
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        webClient = createWebClient();
+        jobsDir = new File(hudson.root, "jobs");
     }
 
     public void testCreation() throws IOException, SAXException {
         createFreeStyleProject("newjob");
+        final List<File> historyFiles = Arrays.asList(new File(jobsDir, "newjob/config-history").listFiles());
+        assertEquals(historyFiles.toString(), 1, historyFiles.size());
     }
 
     public void testRename() throws IOException, SAXException {
         final FreeStyleProject project = createFreeStyleProject("newjob");
-        project.renameTo("renamedob");
-        project.save();
+        project.renameTo("renamedjob");
+        final File[] historyFiles = new File(jobsDir, "newjob/config-history").listFiles();
+        assertNull("Got history files for old job", historyFiles);
+        final List<File> historyFilesNew = Arrays.asList(new File(jobsDir, "renamedjob/config-history").listFiles());
+        assertEquals(historyFilesNew.toString(), 1, historyFilesNew.size());
     }
 }
