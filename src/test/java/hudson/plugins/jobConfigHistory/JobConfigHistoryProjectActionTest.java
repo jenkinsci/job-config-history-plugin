2946344680be765e1152ee15a5dfdc8d5ef2c813
mfriedenhagen <mfriedenhagen@dev.java.net>
2010-01-27 20:48:52 +0000

    Diff tests.

--- src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryProjectActionTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryProjectActionTest.java
@@ -12,14 +12,29 @@ import junit.framework.TestCase;
  */
 public class JobConfigHistoryProjectActionTest extends TestCase {
 
+    private final JobConfigHistoryProjectAction action = new JobConfigHistoryProjectAction(null);
+
     /**
      * Test method for {@link hudson.plugins.jobConfigHistory.JobConfigHistoryProjectAction#getDiffFile(java.lang.String, java.lang.String)}.
      */
-    public void testGetDiffFileStringString() {
-        final JobConfigHistoryProjectAction action = new JobConfigHistoryProjectAction(null);
+    public void testGetDiffFileStringStringSameLineLength() {
         final String s1 = "123\n346";
         final String s2 = "123\n3467";
         assertEquals("\nDiffs:\n\n< 346\n> 3467\n", action.getDiff(s1.split("\n"), s2.split("\n")));
     }
 
+    /**
+     * Test method for {@link hudson.plugins.jobConfigHistory.JobConfigHistoryProjectAction#getDiffFile(java.lang.String, java.lang.String)}.
+     */
+    public void testGetDiffFileStringStringEmpty() {
+        assertEquals("\nDiffs:\n\n", action.getDiff(null, null));
+        assertEquals("\nDiffs:\n\n", action.getDiff(new String[0], new String[0]));
+    }
+    /**
+     * Test method for {@link hudson.plugins.jobConfigHistory.JobConfigHistoryProjectAction#getDiffFile(java.lang.String, java.lang.String)}.
+     */
+    public void testGetDiffFileStringStringDifferentLineLength() {
+        assertEquals("\nDiffs:\n\n", action.getDiff("123\n346".split("\n"), "123\n346\n".split("\n")));
+        assertEquals("\nDiffs:\n\n> 123\n", action.getDiff("123\n346".split("\n"), "123\n346\n123".split("\n")));
+    }
 }
