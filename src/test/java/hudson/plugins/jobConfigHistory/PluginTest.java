2ea886a6ca9a14986c4e4b0a3d6936d59aceec2b
mfriedenhagen <mfriedenhagen@dev.java.net>
2010-01-28 21:08:52 +0000

    Use pageobjects in test.

--- src/test/java/hudson/plugins/jobConfigHistory/PluginTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/PluginTest.java
@@ -14,12 +14,10 @@ import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 import org.xml.sax.SAXException;
 
-import com.gargoylesoftware.htmlunit.Page;
 import com.gargoylesoftware.htmlunit.TextPage;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlButton;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlRadioButtonInput;
 import com.gargoylesoftware.htmlunit.html.HtmlTextArea;
@@ -30,9 +28,111 @@ import com.gargoylesoftware.htmlunit.xml.XmlPage;
  */
 public class PluginTest extends HudsonTestCase {
 
+    private class JobPage {
+
+        private final HtmlPage jobPage;
+
+        private final String jobConfigHistoryLink;
+
+        public JobPage() throws IOException, SAXException {
+            this.jobPage = webClient.goTo("job/" + JOB_NAME);
+            this.jobConfigHistoryLink = getJobConfigHistoryLink();
+        }
+
+        /**
+         * @return
+         */
+        public String getJobConfigHistoryLink() {
+            return "job/" + JOB_NAME + "/jobConfigHistory";
+        }
+
+        public void checkHistoryLink() {
+            assertXPath(jobPage, "//a[@href=\"/" + jobConfigHistoryLink + "\"]");
+        }
+    }
+
+    private class ConfigPage {
+
+        private final HtmlPage configPage;
+
+        /**
+         * @param configPage
+         * @throws SAXException
+         * @throws IOException
+         */
+        public ConfigPage() throws IOException, SAXException {
+            this.configPage = webClient.goTo("job/" + JOB_NAME + "/configure");
+        }
+
+        public void setNewDescription(final String text) throws Exception {
+            final HtmlForm configForm = configPage.getFormByName("config");
+            final HtmlTextArea descriptionTextArea = configForm.getTextAreaByName("description");
+            descriptionTextArea.setText(text);
+            submit(configForm);
+        }
+    }
+
+    private class BasicHistoryPage {
+
+        final HtmlPage historyPage;
+
+        public BasicHistoryPage(HtmlPage historyPage) throws IOException, SAXException {
+            this.historyPage = historyPage;
+            assertEquals("Job Configuration History [Hudson]", historyPage.getTitleText());
+        }
+
+        public List<? extends HtmlAnchor> getConfigOutputLinks(final String type) {
+            @SuppressWarnings("unchecked")
+            final List<? extends HtmlAnchor> hrefs = (List<? extends HtmlAnchor>) historyPage
+                    .getByXPath("//a[contains(@href, \"configOutput?type=" + type + "\")]");
+            return hrefs;
+        }
+
+    }
+
+    private class AllJobConfigHistoryPage extends BasicHistoryPage {
+
+        public AllJobConfigHistoryPage(HtmlPage historyPage) throws IOException, SAXException {
+            super(historyPage);
+            assertXPath(historyPage, "//h1[text()=\"All Jobs Configuration History\"]");
+        }
+
+    }
+
+    private class HistoryPage extends BasicHistoryPage {
+
+        public HistoryPage() throws IOException, SAXException {
+            super(webClient.goTo("job/" + JOB_NAME + "/jobConfigHistory"));
+            assertEquals("Job Configuration History [Hudson]", historyPage.getTitleText());
+        }
+
+        public TextPage getDiffPage() throws IOException {
+            final HtmlForm diffFilesForm = historyPage.getFormByName("diffFiles");
+            return (TextPage) diffFilesForm.submit((HtmlButton) last(diffFilesForm.getHtmlElementsByTagName("button")));
+        }
+
+        public void setCheckedDiffFile1RadioButton(int index, boolean isChecked) {
+            getDiffFileRadioButton("DiffFile1", index).setChecked(isChecked);
+        }
+
+        public void setCheckedDiffFile2RadioButton(int index, boolean isChecked) {
+            getDiffFileRadioButton("DiffFile2", index).setChecked(isChecked);
+        }
+
+        public void assertNoHistoryEntriesAvailable() {
+            assertThat(historyPage.asXml(), containsString("No job configuration history available"));
+        }
+
+        private HtmlRadioButtonInput getDiffFileRadioButton(final String name, int index) {
+            final HtmlForm diffFilesForm = historyPage.getFormByName("diffFiles");
+            return (HtmlRadioButtonInput) diffFilesForm.getInputsByName(name).get(index);
+        }
+
+    }
+
     private WebClient webClient;
 
-    private static final String JOB_CONFIG_HISTORY_LINK = "job/bar/jobConfigHistory";
+    private static final String JOB_NAME = "bar";
 
     @Override
     protected void setUp() throws Exception {
@@ -41,99 +141,53 @@ public class PluginTest extends HudsonTestCase {
     }
 
     public void testAllProjectsConfigurationHistoryPage() throws IOException, SAXException {
-        final HtmlPage allProjectsHistory = webClient.goTo("jobConfigHistory/");
-        assertEquals("Job Configuration History [Hudson]", allProjectsHistory.getTitleText());
-        assertXPath(allProjectsHistory, "//h1[text()=\"All Jobs Configuration History\"]");
+        new AllJobConfigHistoryPage(webClient.goTo("jobConfigHistory/"));
     }
 
     @LocalData
     public void testJobPage() throws IOException, SAXException {
-        final HtmlPage jobPage = webClient.goTo("job/bar/");
-        assertXPath(jobPage, "//a[@href=\"/" + JOB_CONFIG_HISTORY_LINK + "\"]");
-        goToJobConfigurationHistoryPage(jobPage);
+        new JobPage().checkHistoryLink();
     }
 
     @LocalData
     public void testHistoryPageWithOutEntries() throws IOException, SAXException {
-        final HtmlPage historyPage = webClient.goTo(JOB_CONFIG_HISTORY_LINK);
-        final String html = historyPage.asXml();
-        assertThat(html, containsString("No job configuration history available"));
-    }
-    /**
-     * @param jobPage
-     * @throws IOException
-     */
-    private HtmlPage goToJobConfigurationHistoryPage(final HtmlPage jobPage) throws IOException {
-        final HtmlPage jobConfigHistoryPage = (HtmlPage) jobPage.getAnchorByHref("/" + JOB_CONFIG_HISTORY_LINK).click();
-        assertEquals("Job Configuration History [Hudson]", jobConfigHistoryPage.getTitleText());
-        return jobConfigHistoryPage;
+        new HistoryPage().assertNoHistoryEntriesAvailable();
     }
 
     @LocalData
     public void testSaveConfiguration() throws Exception {
         final String firstDescription = "just a test";
-        final List<? extends HtmlAnchor> hrefs1 = setNewDescriptionAndGetHistoryLinks(firstDescription, "xml");
-        assertEquals(1, hrefs1.size());
-        final HtmlAnchor xmlAnchor = hrefs1.get(0);
-        final XmlPage firstXml = (XmlPage) xmlAnchor.click();
-        assertThat(firstXml.asXml(), containsString(firstDescription));
         final String secondDescription = "just a second test";
-        final List<? extends HtmlAnchor> hrefs2 = setNewDescriptionAndGetHistoryLinks(secondDescription, "raw");
-        assertEquals(2, hrefs2.size());
-        final HtmlAnchor rawAnchor = hrefs2.get(0);
-        final TextPage firstRaw = (TextPage) rawAnchor.click();
-        assertThat(firstRaw.getContent(), containsString(secondDescription));
-        final HtmlPage allProjectsHistory = webClient.goTo("jobConfigHistory/");
-        assertEquals("Job Configuration History [Hudson]", allProjectsHistory.getTitleText());
-        assertXPath(allProjectsHistory, "//h1[text()=\"All Jobs Configuration History\"]");
-        List<? extends HtmlAnchor> allXmlHRefs = getConfigOutputLinks("raw", allProjectsHistory);
+        {
+            new ConfigPage().setNewDescription(firstDescription);
+            final List<? extends HtmlAnchor> hrefs = new HistoryPage().getConfigOutputLinks("xml");
+            assertEquals(1, hrefs.size());
+            final HtmlAnchor xmlAnchor = hrefs.get(0);
+            final XmlPage xmlPage = (XmlPage) xmlAnchor.click();
+            assertThat(xmlPage.asXml(), containsString(firstDescription));
+        }
+        {
+            new ConfigPage().setNewDescription(secondDescription);
+            final List<? extends HtmlAnchor> hrefs = new HistoryPage().getConfigOutputLinks("raw");
+            assertEquals(2, hrefs.size());
+            final HtmlAnchor rawAnchor = hrefs.get(0);
+            final TextPage firstRaw = (TextPage) rawAnchor.click();
+            assertThat(firstRaw.getContent(), containsString(secondDescription));
+        }
+        final AllJobConfigHistoryPage allJobConfigHistoryPage = new AllJobConfigHistoryPage(webClient.goTo("jobConfigHistory/"));
+        List<? extends HtmlAnchor> allRawHRefs = allJobConfigHistoryPage.getConfigOutputLinks("raw");
+        assertEquals(2, allRawHRefs.size());
+        List<? extends HtmlAnchor> allXmlHRefs = allJobConfigHistoryPage.getConfigOutputLinks("xml");
         assertEquals(2, allXmlHRefs.size());
-        final TextPage firstRawOfAll = (TextPage) allXmlHRefs.get(0).click();
+        final TextPage firstRawOfAll = (TextPage) allRawHRefs.get(0).click();
         assertThat(firstRawOfAll.getContent(), containsString(secondDescription));
-        final HtmlPage jobConfigHistoryPage = webClient.goTo(JOB_CONFIG_HISTORY_LINK);
-        final HtmlForm diffFilesForm = jobConfigHistoryPage.getFormByName("diffFiles");
-        final HtmlRadioButtonInput diffFile1 = (HtmlRadioButtonInput) diffFilesForm.getInputsByName("DiffFile1").get(0);
-        final HtmlRadioButtonInput diffFile2 = (HtmlRadioButtonInput) diffFilesForm.getInputsByName("DiffFile2").get(1);
-        diffFile1.setChecked(true);
-        diffFile2.setChecked(true);
-        final TextPage diffPage = (TextPage) diffFilesForm.submit((HtmlButton)last(diffFilesForm.getHtmlElementsByTagName("button")));
-        final String diffPageContent = diffPage.getContent();
+        final HistoryPage historyPage = new HistoryPage();
+        historyPage.setCheckedDiffFile1RadioButton(0, true);
+        historyPage.setCheckedDiffFile2RadioButton(1, true);
+        final String diffPageContent = historyPage.getDiffPage().getContent();
         assertThat(diffPageContent, containsString("Diffs:"));
-        assertThat(diffPageContent, containsString("<   <description>just a second test</description>"));
-        assertThat(diffPageContent, containsString(">   <description>just a test</description>"));
-    }
-
-    /**
-     * @param text
-     * @param type
-     * @return
-     * @throws IOException
-     * @throws SAXException
-     * @throws Exception
-     */
-    private List<? extends HtmlAnchor> setNewDescriptionAndGetHistoryLinks(final String text, final String type)
-            throws IOException, SAXException, Exception {
-        final HtmlPage configuration = webClient.goTo("job/bar/configure");
-        assertXPath(configuration, "//a[@href=\"/" + JOB_CONFIG_HISTORY_LINK + "\"]");
-        final HtmlForm configForm = configuration.getFormByName("config");
-        final HtmlTextArea descriptionTextArea = configForm.getTextAreaByName("description");
-        descriptionTextArea.setText(text);
-        final HtmlPage jobPage = submit(configForm);
-        final HtmlPage historyPage = goToJobConfigurationHistoryPage(jobPage);
-        final List<? extends HtmlAnchor> hrefs = getConfigOutputLinks(type, historyPage);
-        return hrefs;
-    }
-
-    /**
-     * @param type
-     * @param historyPage
-     * @return
-     */
-    private List<? extends HtmlAnchor> getConfigOutputLinks(final String type, final HtmlPage historyPage) {
-        @SuppressWarnings("unchecked")
-        final List<? extends HtmlAnchor> hrefs = (List<? extends HtmlAnchor>) historyPage
-                .getByXPath("//a[contains(@href, \"configOutput?type=" + type + "\")]");
-        return hrefs;
+        assertThat(diffPageContent, containsString("<   <description>" + secondDescription + "</description>"));
+        assertThat(diffPageContent, containsString(">   <description>" + firstDescription + "</description>"));
     }
 
 }
