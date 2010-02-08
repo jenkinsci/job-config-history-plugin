8e7d542e8577251e9c7b5c2738b872cda803e626
mfriedenhagen <mfriedenhagen@dev.java.net>
2010-02-08 22:19:58 +0000

    Implement testcase for HUDSON-5534, which shows the jobConfigHistory-badge only if security is not enabled.

--- src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryBaseActionTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryBaseActionTest.java
@@ -4,10 +4,20 @@
 
 package hudson.plugins.jobConfigHistory;
 
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.containsString;
+import hudson.security.HudsonPrivateSecurityRealm;
+import hudson.security.LegacyAuthorizationStrategy;
+
 import java.io.IOException;
 
+import org.jvnet.hudson.test.Bug;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.xml.sax.SAXException;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 /**
  * @author mfriedenhagen
@@ -15,8 +25,17 @@ import org.xml.sax.SAXException;
  */
 public class JobConfigHistoryBaseActionTest extends HudsonTestCase {
 
+    private WebClient webClient;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        webClient = createWebClient();
+    }
+
     /**
-     * Test method for {@link hudson.plugins.jobConfigHistory.JobConfigHistoryBaseAction#getConfigXml(java.lang.String)}.
+     * Test method for {@link hudson.plugins.jobConfigHistory.JobConfigHistoryBaseAction#getConfigXml(java.lang.String)}
+     * .
      *
      * @throws SAXException
      * @throws IOException
@@ -35,6 +54,25 @@ public class JobConfigHistoryBaseActionTest extends HudsonTestCase {
             fail("Expected " + IllegalArgumentException.class);
         } catch (IllegalArgumentException e) {
             System.err.println(e);
+        }
+    }
+
+    @Bug(5534)
+    public void testSecuredAccessToJobConfigHistoryPage() throws IOException, SAXException {
+        // without security the jobConfigHistory-badge should show.
+        final HtmlPage withoutSecurity = webClient.goTo("/");
+        assertThat(withoutSecurity.asXml(), containsString(JobConfigHistoryConsts.ICONFILENAME));
+        withoutSecurity.getAnchorByHref("/" + JobConfigHistoryConsts.URLNAME);
+        // with security enabled the jobConfigHistory-badge should not show anymore.
+        hudson.setSecurityRealm(new HudsonPrivateSecurityRealm(false));
+        hudson.setAuthorizationStrategy(new LegacyAuthorizationStrategy());
+        final HtmlPage withSecurityEnabled = webClient.goTo("/");
+        assertThat(withSecurityEnabled.asXml(), not(containsString(JobConfigHistoryConsts.ICONFILENAME)));
+        try {
+            withSecurityEnabled.getAnchorByHref("/" + JobConfigHistoryConsts.URLNAME);
+            fail("Expected a " + ElementNotFoundException.class + " to be thrown");
+        } catch (ElementNotFoundException e) {
+            // Expected
         }
     }
 
