1d55c9cc4d13a44c4d75cee938c84f0a05cda6cc
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-08-27 20:37:22 +0200

    Add one unit test.

---
+++ src/test/java/hudson/plugins/jobConfigHistory/ConfigInfoComparatorTest.java
@@ -0,0 +1,37 @@
+package hudson.plugins.jobConfigHistory;
+
+import hudson.model.AbstractItem;
+import hudson.model.ItemGroup;
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author Mirko Friedenhagen
+ */
+public class ConfigInfoComparatorTest {
+
+    /**
+     * Test of compare method, of class ConfigInfoComparator.
+     */
+    @Test
+    public void testCompare() throws UnsupportedEncodingException {
+        ItemGroup itemGroupMock = mock(ItemGroup.class);
+        when(itemGroupMock.getFullName()).thenReturn("does not matter");
+        AbstractItem itemMock = mock(AbstractItem.class);
+        when(itemMock.getParent()).thenReturn(itemGroupMock);
+        when(itemMock.getFullName()).thenReturn("does not matter");
+        HistoryDescr descrMock = mock(HistoryDescr.class);
+        when(descrMock.getTimestamp()).thenReturn("123");
+        ConfigInfo ci1 = ConfigInfo.create(itemMock, new File(""), descrMock);
+        ConfigInfo ci2 = ConfigInfo.create(itemMock, new File(""), descrMock);
+        int expResult = 0;
+        int result = ConfigInfoComparator.INSTANCE.compare(ci1, ci2);
+        assertEquals(expResult, result);
+    }
+
+}
