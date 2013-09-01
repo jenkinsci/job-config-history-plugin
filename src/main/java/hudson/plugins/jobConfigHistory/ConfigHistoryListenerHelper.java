31360cf79fdca9373d38997e85534cb4cd2cac63
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-09-01 22:42:35 +0200

    Remove unused enum.

--- src/main/java/hudson/plugins/jobConfigHistory/ConfigHistoryListenerHelper.java
+++ src/main/java/hudson/plugins/jobConfigHistory/ConfigHistoryListenerHelper.java
@@ -9,43 +9,6 @@ import hudson.XmlFile;
 public interface ConfigHistoryListenerHelper {
 
     /**
-     * Possible Events.
-     */
-    public enum Events {
-
-        /** Job created. */
-        CREATED(Messages.ConfigHistoryListenerHelper_CREATED()),
-
-        /** Job renamed. */
-        RENAMED(Messages.ConfigHistoryListenerHelper_RENAMED()),
-
-        /** Job modified. */
-        CHANGED(Messages.ConfigHistoryListenerHelper_CHANGED()),
-
-        /** Job deleted. */
-        DELETED(Messages.ConfigHistoryListenerHelper_DELETED());
-
-        /**
-         * Name of the operation.
-         */
-        private final String operation;
-
-        /**
-         * @param operation localized version.
-         */
-        Events(final String operation) {
-            this.operation = operation;
-        }
-
-        /**
-         * @return the operation
-         */
-        public String getOperation() {
-            return operation;
-        }
-    }
-
-    /**
      * Creates a new backup of the job configuration.
      *
      * @param xmlFile
