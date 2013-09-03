bac033d4bd5043fecbbe56effbe891ae2226c4bc
Mirko Friedenhagen <mfriedenhagen@gmail.com>
2013-09-03 11:14:47 +0200

    Create UnpackResourceZip for testing config file directories without mocking everything.

---
+++ src/test/java/hudson/plugins/jobConfigHistory/UnpackResourceZip.java
@@ -0,0 +1,81 @@
+package hudson.plugins.jobConfigHistory;
+
+import hudson.util.IOUtils;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.TemporaryFolder;
+
+/**
+ * Takes the URL of zip with test resources and unpacks it to a temporary folder, which
+ * may be retrieved with {@link UnpackResourceZip#getRoot()}.
+ *
+ * @author Mirko Friedenhagen
+ */
+public class UnpackResourceZip extends ExternalResource {
+
+    private final TemporaryFolder temporaryFolder = new TemporaryFolder(new File("target"));
+
+    private final URL resourceURL;
+
+    public UnpackResourceZip(URL resourceURL) {
+        this.resourceURL = resourceURL;
+    }
+
+    @Override
+    protected void before() throws Throwable {
+        temporaryFolder.create();
+        unpackZip();
+    }
+
+    @Override
+    protected void after() {
+        temporaryFolder.delete();
+    }
+
+    /**
+     * @return the location of the unpacked resources.
+     */
+    public File getRoot() {
+        return temporaryFolder.getRoot();
+    }
+
+    private void unpackZip() throws IOException {
+        // IOUtils.copy below buffers InputStream!
+        final ZipInputStream in = new ZipInputStream(resourceURL.openStream());
+        try {
+            ZipEntry entry = in.getNextEntry();
+            while (entry != null) {
+                final File file = new File(temporaryFolder.getRoot(), entry.getName());
+                if (!entry.isDirectory()) {
+                    createFileResource(file, in);
+                } else {
+                    createDirectoryResource(file);
+                }
+                entry = in.getNextEntry();
+            }
+        } finally {
+            in.close();
+        }
+    }
+
+    private void createFileResource(final File file, final ZipInputStream in) throws FileNotFoundException, IOException {
+        createDirectoryResource(file.getParentFile());
+        final BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file));
+        try {
+            IOUtils.copy(in, out);
+        } finally {
+            out.close();
+        }
+    }
+
+    private void createDirectoryResource(final File file) {
+        file.mkdirs();
+    }
+}
