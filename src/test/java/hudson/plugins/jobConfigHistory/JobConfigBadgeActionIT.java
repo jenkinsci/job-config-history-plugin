e01fc680459c54e90db73566ba09be8ce4ca4ece
Stefan Brausch <stefan.brausch@1und1.de>
2014-01-24 12:56:34 +0100

    Failing IT's fixed

--- src/test/java/hudson/plugins/jobConfigHistory/JobConfigBadgeActionIT.java
+++ src/test/java/hudson/plugins/jobConfigHistory/JobConfigBadgeActionIT.java
@@ -83,7 +83,7 @@ public class JobConfigBadgeActionIT extends AbstractHudsonTestCaseDeletingInstan
         final HtmlPage htmlPage = webClient.goTo("job/" + oldJobName);
         final HtmlAnchor showDiffLink = (HtmlAnchor) htmlPage.getElementById("showDiff");
         final HtmlPage showDiffPage = showDiffLink.click();
-        assertTrue("ShowDiffFiles page should be reached now", showDiffPage.asText().contains("Restore old version"));
+        assertTrue("ShowDiffFiles page should be reached now", showDiffPage.asText().contains("Restore file A configuration"));
 
         project.renameTo(newJobName);
         Thread.sleep(SLEEP_TIME);
@@ -93,7 +93,7 @@ public class JobConfigBadgeActionIT extends AbstractHudsonTestCaseDeletingInstan
         final HtmlPage htmlPage2 = webClient.goTo("job/" + newJobName);
         final HtmlAnchor oldShowDiffLink = (HtmlAnchor) htmlPage2.getByXPath("//a[@id='showDiff']").get(1);
         final HtmlPage showDiffPage2 = oldShowDiffLink.click();
-        assertTrue("ShowDiffFiles page should be reached now", showDiffPage2.asText().contains("Restore old version"));
+        assertTrue("ShowDiffFiles page should be reached now", showDiffPage2.asText().contains("Restore file A configuration"));
     }
     
     public void testProjectWithConfigsButMissingBuilds() throws Exception {
