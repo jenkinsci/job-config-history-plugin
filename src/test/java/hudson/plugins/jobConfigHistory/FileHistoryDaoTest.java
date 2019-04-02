00335b41b2b8bfb439b442783da33aba18a8df23
fbelzunc@gmail.com <fbelzunc@gmail.com>
2019-04-02 16:44:53 +0200

    [JENKINS-41844] Avoid the unused import java.io.FilenameFilter

--- src/test/java/hudson/plugins/jobConfigHistory/FileHistoryDaoTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/FileHistoryDaoTest.java
@@ -40,7 +40,6 @@ import static org.mockito.Mockito.when;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
