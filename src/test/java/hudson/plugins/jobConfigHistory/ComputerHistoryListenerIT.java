112b6fa8d0d634d38801f632be33df920a2bf666
René Scheibe <rene.scheibe@gmail.com>
2019-04-14 17:39:18 +0200

    Improvement - Remove useless autogenerated documentation

--- src/test/java/hudson/plugins/jobConfigHistory/ComputerHistoryListenerIT.java
+++ src/test/java/hudson/plugins/jobConfigHistory/ComputerHistoryListenerIT.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package hudson.plugins.jobConfigHistory;
 
 import static org.junit.Assert.assertEquals;
