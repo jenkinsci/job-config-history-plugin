0eaa24e3a15110fc1f8293dda3e20f66c6175414
Robin Schulz <robin.schulz@1und1.de>
2019-09-05 12:12:08 +0200

    code review #2

--- src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryRootActionTest.java
+++ src/test/java/hudson/plugins/jobConfigHistory/JobConfigHistoryRootActionTest.java
@@ -23,7 +23,13 @@
  */
 package hudson.plugins.jobConfigHistory;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+import static org.junit.Assert.assertFalse;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
