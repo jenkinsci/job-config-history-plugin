91fe1caa79fc1f331baf66830dd989116a08e426
Robin Schulz <robin.schulz@1und1.de>
2019-12-30 12:23:09 +0100

    Implement warning and improve overall design.

--- src/main/java/hudson/plugins/jobConfigHistory/JobLocalConfiguration.java
+++ src/main/java/hudson/plugins/jobConfigHistory/JobLocalConfiguration.java
@@ -2,15 +2,28 @@ package hudson.plugins.jobConfigHistory;
 
 import hudson.Extension;
 import hudson.model.AbstractProject;
+import hudson.model.Item;
 import hudson.model.Job;
 import hudson.model.JobProperty;
 import hudson.model.JobPropertyDescriptor;
+import hudson.util.FormValidation;
+import jenkins.model.Jenkins;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.DoNotUse;
+import org.kohsuke.stapler.AncestorInPath;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.interceptor.RequirePOST;
+
+import java.text.Normalizer;
 
 public class JobLocalConfiguration extends JobProperty<Job<?, ?>> {
-
-	@Extension
-	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+//
+//	@Extension
+//	public static final DescriptorImpl DESCRIPTOR = new DescriptorImpl();
 
 
 	//TODO maybe optionally mandatory (configurable globally!)
@@ -20,17 +33,26 @@ public class JobLocalConfiguration extends JobProperty<Job<?, ?>> {
 	@DataBoundConstructor
 	public JobLocalConfiguration(String  changeReasonComment) {
 		System.out.println("crc: " + changeReasonComment);
+
 		this.changeReasonComment = changeReasonComment;
 	}
 
-	@Override
-	public DescriptorImpl getDescriptor() {
-		return DESCRIPTOR;
+	public String getChangeReasonComment() {
+		return changeReasonComment;
 	}
+//
+//	@Override
+//	public DescriptorImpl getDescriptor() {
+//		return DESCRIPTOR;
+//	}
+//
+
 
 	/** Constructor loads previously saved form data.  TODO maybe not do this.*/
-	public static final class DescriptorImpl extends JobPropertyDescriptor {
-		DescriptorImpl() {
+	@Extension
+	public static class DescriptorImpl extends JobPropertyDescriptor {
+
+		public DescriptorImpl() {
 			super(JobLocalConfiguration.class);
 			System.out.println("DESCRIPTOR IMPLE");
 			load();
@@ -42,8 +64,20 @@ public class JobLocalConfiguration extends JobProperty<Job<?, ?>> {
 		}
 
 		public boolean isApplicable(AbstractProject<?, ?> item) {
-			System.out.println("ISAPPLICABLE not yet implemented! " + item.getFullName());
 			return true;
 		}
+
+		public boolean configure(StaplerRequest request, JSONObject jsonObject) throws FormException {
+			System.out.println("CONFIGURE");
+			throw new FormException("form exception", "localValues.changeReasonComment");
+		}
+
+
+		public FormValidation doCheckChangeReasonComment(@QueryParameter String changeReasonComment, @AncestorInPath Item item) {
+			//TODO maybe use this instead of javascript. (need to figure out how to relocate the message...)
+			return FormValidation.ok();
+		}
+
 	}
+
 }
