c123a55a8a395b7f9f54586b0455ce2ff1b8a270
Shlomo Dahan <64103471+shlomomdahan@users.noreply.github.com>
2024-10-10 13:53:03 -0400

    [JENKINS-73883] Un-inline JavaScript in `history.jelly` and `index.jelly` (#338)

---
+++ src/main/resources/hudson/plugins/jobConfigHistory/deleteRevisionAndTableEntry.js
@@ -0,0 +1,46 @@
+//name is an optional parameter for job deletion history and system config history
+function removeEntryFromTable(id, timestamp, name, message) {
+    var confirmPhrase = message + timestamp + '?';
+    if (confirm(confirmPhrase)) {
+        var tableRow = document.getElementById(id);
+        tableRow.parentNode.removeChild(tableRow);
+
+        //redirect
+        var xmlHttp = new XMLHttpRequest();
+        var url = 'deleteRevision?timestamp=' + timestamp;
+        if (name != null) {
+            url += "&name=" + name;
+        }
+        xmlHttp.open("POST", url, true);
+        xmlHttp.setRequestHeader(document.head.getAttribute('data-crumb-header'), document.head.getAttribute('data-crumb-value'));
+        xmlHttp.send(null);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const targetDiv = document.querySelector('#target-div');
+    let jobName = null;
+    if (targetDiv) {
+        jobName = targetDiv.getAttribute('jobName');
+    }
+
+    const systemConfigDeleteButtons = document.querySelectorAll('.system-config-history-delete-button');
+    systemConfigDeleteButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const configNr = button.getAttribute('data-config-nr');
+            const configDate = button.getAttribute('data-config-date');
+            const message = button.getAttribute('data-message-text');
+            removeEntryFromTable(`table-row-${configNr}`, configDate, jobName, message);
+        });
+    });
+
+    const agentConfigDeleteButtons = document.querySelectorAll('.agent-config-history-delete-button');
+    agentConfigDeleteButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const configNr = button.getAttribute('data-config-nr');
+            const configDate = button.getAttribute('data-config-date');
+            const message = button.getAttribute('data-message-text');
+            removeEntryFromTable(`table-row-${configNr}`, configDate, null, message);
+        });
+    });
+});
