ebdfb00b5ec09ccadf7df417de9adbe25936221f
Yaroslav <91559310+yaroslavafenkin@users.noreply.github.com>
2024-10-29 20:06:42 +0200

    [JENKINS-74032][JENKINS-73033][JENKINS-73034] Improve CSP compatibility (#343)
    Co-authored-by: Basil Crow <me@basilcrow.com>

---
+++ src/main/resources/hudson/plugins/jobConfigHistory/show-diff-files-behaviour.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", () => {
+    hljs.initHighlightingOnLoad();
+
+    var showVersionDiffsJs = true;
+
+    function toggleShowHideVersionDiffsJs(button, hideText, showText) {
+        showVersionDiffsJs = !showVersionDiffsJs;
+        if (showVersionDiffsJs === true) {
+            document.getElementById("tbody_versionDiffsShown").style.display = "";
+            document.getElementById("tbody_versionDiffsHidden").style.display = "none";
+
+            button.value = hideText;
+        } else {
+            document.getElementById("tbody_versionDiffsShown").style.display = "none";
+            document.getElementById("tbody_versionDiffsHidden").style.display = "";
+
+            button.value = showText;
+        }
+    }
+
+    document.querySelector("#showHideVersionDiffsJsButton").addEventListener("click", (event) => {
+        const { hideText, showText } = event.target.dataset;
+
+        toggleShowHideVersionDiffsJs(event.target, hideText, showText);
+    });
+});
